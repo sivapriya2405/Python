0920f57fb0539581542056fb54a1fd7ea0f30eca
sivapriya2405 <93479453+sivapriya2405@users.noreply.github.com>
2021-11-07 18:52:21 +0530

    Add files via upload

---
+++ Nov 6 Task - String Indexing.py
@@ -0,0 +1,27 @@
+a="welcome*to#python&world!keep_learning^everyday"
+
+print(a[20]) #r
+print(a[-16])#e
+print(a[21])#l
+print(a[-15])#a
+print(a[23])#!
+print(a[-13])#n
+print(a[31])#a
+print(a[-5])#r
+print(a[22])#d
+print(a[-14])#r
+print(a[30])#e
+print(a[-6])#e
+print(a[11])#p
+print(a[-25])#l
+
+
+print(a[7])#*
+print(a[-39])#*
+print(a[11])#p
+print(a[-35])#p
+print(a[17])#&
+print(a[-29])#&
+print(a[37])#^
+print(a[-9])#^
+
