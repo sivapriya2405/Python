00f11d22daf58f2d49913abc9708b5d7aaa45396
sivapriya2405 <93479453+sivapriya2405@users.noreply.github.com>
2021-11-12 20:44:08 +0530

    Add files via upload

---
+++ slice.py
@@ -0,0 +1,14 @@
+a="computer_science"
+print(a[1:10:3])
+print(a[10:1:3])
+print(a[10:1:-3])
+print(a[1:10:-3])
+
+print(a[:10:5])# no starting number so interval (3rd parameter )will decide which direction it need to proceed
+print(a[:10:-5])
+print(a[5::5])
+print(a[5::-5])
+print(a[-5::-5])
+print(a[-5::5])
+print(a[:-10:5])
+print(a[:-10:-5])
